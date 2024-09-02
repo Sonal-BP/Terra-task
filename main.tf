72cb22fd9eca95e8fb5b6d29cbf55a5e02a80f44
Ubuntu <ubuntu@ip-172-31-25-155.ec2.internal>
2024-09-02 10:34:21 +0000

    upcommit

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
 provider "aws" {
-  region = var.aws_region
+  region     = "us-west-2"  # Adjust as needed
+ 
 }
 
 # VPC
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr_block
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "my-vpc"
+    Name = "main-vpc"
   }
 }
 
 # Public Subnets
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_1_cidr
-  availability_zone = var.public_subnet_1_az
+  cidr_block        = var.public_subnet_cidrs[0]
+  availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1"
@@ -25,8 +26,8 @@ resource "aws_subnet" "public_subnet_1" {
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_2_cidr
-  availability_zone = var.public_subnet_2_az
+  cidr_block        = var.public_subnet_cidrs[1]
+  availability_zone = "us-west-2b"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-2"
@@ -36,8 +37,8 @@ resource "aws_subnet" "public_subnet_2" {
 # Private Subnets
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_1_cidr
-  availability_zone = var.private_subnet_1_az
+  cidr_block        = var.private_subnet_cidrs[0]
+  availability_zone = "us-west-2c"
   tags = {
     Name = "private-subnet-1"
   }
@@ -45,30 +46,34 @@ resource "aws_subnet" "private_subnet_1" {
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_2_cidr
-  availability_zone = var.private_subnet_2_az
+  cidr_block        = var.private_subnet_cidrs[1]
+  availability_zone = "us-west-2d"
   tags = {
     Name = "private-subnet-2"
   }
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "main-igw"
   }
 }
 
-# Route Table for Public Subnets
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
   }
 }
 
+# Public Route Table Association for Public Subnets
 resource "aws_route_table_association" "public_subnet_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public.id
@@ -80,7 +85,7 @@ resource "aws_route_table_association" "public_subnet_2" {
 }
 
 # Security Groups
-resource "aws_security_group" "alb_sg" {
+resource "aws_security_group" "allow_all" {
   vpc_id = aws_vpc.main.id
   egress {
     from_port   = 0
@@ -95,12 +100,10 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "alb-sg"
+    Name = "allow_all"
   }
-}
 
-resource "aws_security_group" "ec2_sg" {
-  vpc_id = aws_vpc.main.id
+  #Another inbound & outbound rules
   egress {
     from_port   = 0
     to_port     = 0
@@ -108,13 +111,10 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "ec2-sg"
   }
 }
 
@@ -123,7 +123,7 @@ resource "aws_lb" "app_lb" {
   name               = "app-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.allow_all.id]
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   enable_deletion_protection = false
   enable_cross_zone_load_balancing = true
@@ -140,40 +140,38 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.app_target_group.arn
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Service is up"
+      status_code  = "200"
+    }
   }
 }
 
-# ALB Target Group
-resource "aws_lb_target_group" "app_target_group" {
-  name     = "app-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-# Launch Configuration
+# Auto Scaling Launch Configuration
 resource "aws_launch_configuration" "app" {
-  image_id       = var.ami_id
-  instance_type  = var.instance_type
-  security_groups = [aws_security_group.ec2_sg.id]
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  name_prefix          = "app-"
+  image_id             = "ami-0075013580f6322a1"  
+  instance_type        = "t2.micro"
+  security_groups      = [aws_security_group.allow_all.id]
+  user_data            = <<-EOF
+                            #!/bin/bash
+                            sudo apt-get update
+                            sudo apt-get install -y nginx
+                            sudo systemctl start nginx
+                            sudo systemctl enable nginx
+                            # Add Tomcat installation here
+                            EOF
 }
 
 # Auto Scaling Group
-resource "aws_autoscaling_group" "app_asg" {
+resource "aws_autoscaling_group" "app" {
   launch_configuration = aws_launch_configuration.app.id
-  min_size             = var.min_size
-  max_size             = var.max_size
-  desired_capacity     = var.desired_capacity
-  vpc_zone_identifier  = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-  health_check_type    = "EC2"
-  health_check_grace_period = 300
-
+  vpc_zone_identifier = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  min_size             = 1
+  max_size             = 3
+  desired_capacity     = 1
   tag {
     key                 = "Name"
     value               = "app-instance"
@@ -181,23 +179,19 @@ resource "aws_autoscaling_group" "app_asg" {
   }
 }
 
-# Auto Scaling Policies
+# Auto Scaling Policy
 resource "aws_autoscaling_policy" "scale_out" {
   name                   = "scale-out"
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
-  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.app.name
 }
 
 resource "aws_autoscaling_policy" "scale_in" {
   name                   = "scale-in"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
-  autoscaling_group_name = aws_autoscaling_group.app_asg.name
-}
-
-# Auto Scaling Group Attach ALB
-resource "aws_autoscaling_attachment" "asg_attachment" {
-  autoscaling_group_name = aws_autoscaling_group.app_asg.name
-  lb_target_group_arn    = aws_lb_target_group.app_target_group.arn
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.app.name
 }
--- variables.tf
+++ variables.tf
@@ -3,49 +3,21 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
-variable "vpc_cidr_block" {
+variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_1_cidr" {
-  description = "CIDR block for the first public subnet"
-  default     = "10.0.1.0/24"
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "public_subnet_1_az" {
-  description = "Availability zone for the first public subnet"
-  default     = "us-west-2a"
-}
-
-variable "public_subnet_2_cidr" {
-  description = "CIDR block for the second public subnet"
-  default     = "10.0.2.0/24"
-}
-
-variable "public_subnet_2_az" {
-  description = "Availability zone for the second public subnet"
-  default     = "us-west-2b"
-}
-
-variable "private_subnet_1_cidr" {
-  description = "CIDR block for the first private subnet"
-  default     = "10.0.3.0/24"
-}
-
-variable "private_subnet_1_az" {
-  description = "Availability zone for the first private subnet"
-  default     = "us-west-2a"
-}
-
-variable "private_subnet_2_cidr" {
-  description = "CIDR block for the second private subnet"
-  default     = "10.0.4.0/24"
-}
-
-variable "private_subnet_2_az" {
-  description = "Availability zone for the second private subnet"
-  default     = "us-west-2b"
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "ami_id" {
@@ -55,7 +27,7 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.medium"
+  default     = "t2.micro"
 }
 
 variable "min_size" {
